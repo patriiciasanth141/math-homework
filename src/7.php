21c28f82d7b320529655974abef346172ef6d440
patriiciasanth141 <patriiciasanth141@gmail.com>
2025-03-13 11:37:34 +0000

    Add 7.php

---
+++ src/7.php
@@ -0,0 +1,12 @@
+<?php
+// Random number between 1 and 10
+$randomNumber = rand(1,10);
+
+// Math problem with the number
+$problem = "What is $randomNumber x 3?";
+
+// Correct answer
+$answer = $randomNumber * 3;
+
+echo $problem;
+?>
