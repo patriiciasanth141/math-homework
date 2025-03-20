81e7d8cac680dabe52b0e01bc067f6ee21a22f5e
patriiciasanth141 <patriiciasanth141@gmail.com>
2025-03-20 07:24:06 +0000

    Add 13.php

---
+++ src/13.php
@@ -0,0 +1,10 @@
+ <?php 
+    // Define an array of numbers
+$numbers = array(1, 2, 3, 4, 5);
+
+// Shuffle the array to randomize the order of the numbers
+shuffle($numbers);
+
+// Print out the first number in the array
+echo $numbers[0];
+?>
