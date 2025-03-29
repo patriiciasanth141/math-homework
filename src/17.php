069b0ce674db9f61a96ebaab523e8d86d536a4e3
patriiciasanth141 <patriiciasanth141@gmail.com>
2025-03-29 20:14:35 +0000

    Add 17.php

---
+++ src/17.php
@@ -0,0 +1,7 @@
+<?php
+// Define variables and constants
+$student_name = "John Doe";
+$math_problem = "3 + 5 * 2";
+$total = $student_name . " solved the math problem: " . $math_problem . " in " . time() . " seconds.";
+echo $total;
+?>
