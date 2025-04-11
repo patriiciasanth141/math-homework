be6745d24b9511f02e2342ee152164930ede2c58
patriiciasanth141 <patriiciasanth141@gmail.com>
2025-04-11 11:31:58 +0000

    Add 27.php

---
+++ src/27.php
@@ -0,0 +1,12 @@
+<?php
+// Initialize variables
+$a = 5;
+$b = 3;
+
+// Perform some operations using variables
+$c = $a + $b;
+$d = $c * 2;
+
+// Print the results
+echo "Result of a + b: $a, result is $d\n";
+?>
