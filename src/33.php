70f2faf17680120a980859440683ea52896c3162
patriiciasanth141 <patriiciasanth141@gmail.com>
2025-05-17 08:33:40 +0000

    Add 33.php

---
+++ src/33.php
@@ -0,0 +1,13 @@
+<?php
+// Define the array of students' names
+$students = [
+    "Student1",
+    "Student2",
+    "Student3",
+    "Student4",
+    "Student5"
+];
+
+// Display a message indicating that there are 5 students in the class
+echo "There are 5 students in your class.";
+?>
