f589d7d1abdba7cf17b3d54df40d12a5cafad032
patriiciasanth141 <patriiciasanth141@gmail.com>
2025-05-26 15:29:20 +0000

    Add 41.php

---
+++ src/41.php
@@ -0,0 +1,12 @@
+<?php
+// Sample PHP code to demonstrate basic arithmetic operations and conditional statements.
+$a = 5;
+$b = 3;
+
+echo "The sum of $a and $b is: " . $a + $b . "\n";
+if ($a > $b) {
+    echo "The first number is greater than the second number.\n";
+} else {
+    echo "The first number is not greater than the second number.\n";
+}
+?>
